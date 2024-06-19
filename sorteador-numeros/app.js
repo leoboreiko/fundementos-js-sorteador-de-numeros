a89f0db59d1da2b2c124993e6fa383f5847b2a40
leoboreiko <leoboreiko@gmail.com>
2024-06-19 13:26:16 -0300

    sorteador de numeros

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,43 @@
+function sortear() {
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    let sorteados = [];
+    let numero;
+
+    for (let i = 0; i < quantidade; i++) {//laço de repetição para satisfazer a condição da quantidade de números dentro do array.
+        numero = obterNumeroAleatorio(de, ate);
+        while (sorteados.includes(numero)) { // verifica se não tem nenhum número repetido.
+            numero = obterNumeroAleatorio(de, ate);
+        }
+        sorteados.push(numero);
+    }
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados.sort((a, b) => a - b)}</label>`;
+    alterarStatusBotaoReiniciar();
+}
+
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotaoReiniciar() {
+    let botaoReiniciar = document.getElementById('btn-reiniciar');
+    
+    if (botaoReiniciar.classList.contains('container__botao-desabilitado')) {
+        botaoReiniciar.classList.remove('container__botao-desabilitado');
+        botaoReiniciar.classList.add('container__botao');
+    } else {
+        botaoReiniciar.classList.remove('container__botao');
+        botaoReiniciar.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';    
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>;';
+    alterarStatusBotaoReiniciar();
+}
